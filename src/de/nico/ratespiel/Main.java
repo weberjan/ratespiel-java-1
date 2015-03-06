f360392e2c9f8169beaee45ade21693595c18d8e
Nico Alt <nicoalt@posteo.org>
2015-03-06 14:54:13 +0100

    Use Counter

--- src/de/nico/ratespiel/Main.java
+++ src/de/nico/ratespiel/Main.java
@@ -12,11 +12,13 @@ public class Main {
 	public static void main(String[] args) {
 		String s = "j";
 		while (s.equals("j")) {
+			Counter.resetVersuche();
 			int i = Integer
 					.parseInt(JOptionPane
 							.showInputDialog("Willkommen zum Ratespiel. Geben Sie eine Zahl ein:"));
 			int z = (int) (Math.random() * 100);
 			while (z != i) {
+				Counter.plusEinVersuch();
 				if (z < i)
 					i = Integer
 							.parseInt(JOptionPane
@@ -27,7 +29,12 @@ public class Main {
 							.parseInt(JOptionPane
 									.showInputDialog("Deine Zahl ist größer als die zufällige Zahl. Bitte geben Sie eine neue Zahl ein:"));
 			}
-			s = JOptionPane.showInputDialog("Richtig! Nochmal? (j/n)");
+			String v;
+			if (!Counter.wievieleVersuche().equals("1"))
+				v = Counter.wievieleVersuche() + " Versuche";
+			else
+				v = Counter.wievieleVersuche() + " Versuch";				
+			s = JOptionPane.showInputDialog("Richtig! (" + v + ") Nochmal? (j/n)");
 		}
 	}
 }
